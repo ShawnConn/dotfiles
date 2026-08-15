bc0d1bd2e3986b3dc8a48b67b26cb47b21c1cf30
Shawn Conn <shawn@luciditi.io>
2026-08-15 16:30:25 -0700

    update clear notifications

--- .shell/bin/macos-clear-notifications.sh
+++ .shell/bin/macos-clear-notifications.sh
@@ -15,11 +15,19 @@ BACKUP=true
 VERBOSE=false
 
 # ANSI colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+if [[ -t 1 ]] && [[ -z "${NO_COLOR:-}" ]]; then
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    NC='\033[0m' # No Color
+else
+    RED=''
+    GREEN=''
+    YELLOW=''
+    BLUE=''
+    NC=''
+fi
 
 # Print usage helper
 usage() {
